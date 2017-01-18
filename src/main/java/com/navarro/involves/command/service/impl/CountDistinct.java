2054c7a039b992b81e3b83f8086e5275c52080ed
Navarro Ferreira <navarro.pod@gmail.com>
2017-01-17 22:52:32 -0200

    refactor

--- src/main/java/com/navarro/involves/command/service/impl/CountDistinct.java
+++ src/main/java/com/navarro/involves/command/service/impl/CountDistinct.java
@@ -1,6 +1,5 @@
 package com.navarro.involves.command.service.impl;
 
-import com.navarro.involves.command.enums.CommandTypes;
 import com.navarro.involves.object.dao.interfaces.ObjectDAO;
 import com.navarro.involves.writter.Writer;
 
@@ -13,8 +12,6 @@ public class CountDistinct extends CommandServiceImpl {
 
     @Override
     public void execute(String... queryParams) {
-        String[] strings = super.queryParamCommand(CommandTypes.COUNT_DISTINCT.getLiterals(), queryParams[0]);
-
-        writer.writeInfo(String.valueOf(objectDAO.countDistinct(strings[0])));
+        writer.writeInfo(String.valueOf(objectDAO.countDistinct(queryParams[0])));
     }
 }
